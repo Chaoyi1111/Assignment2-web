a3267fc948ba2c5d2a9746cbd71729cd0b59371b
Chaoyi1111 <2736047167@qq.com>
2026-08-30 02:09:09 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const animation = document.getElementById("ballAnimation");
 console.log("icon:", icon);
 console.log("animation:", animation);
 
+const planetSound = new Audio("./sound/a5.mp3");
+planetSound.volume = 0.5;
+
 icon.addEventListener("mouseenter", function() {
     console.log("MOUSE ENTER");
+
     animation.beginElement();
+
+    planetSound.currentTime = 0;
+    planetSound.play();
 });
