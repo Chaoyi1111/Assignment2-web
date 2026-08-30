a26bac07b3fba7d7865685063f5ff8d91d94ab07
Chaoyi1111 <2736047167@qq.com>
2026-08-30 12:48:18 +1000

    Add click event to icon for animation and sound

--- script.js
+++ script.js
@@ -9,9 +9,20 @@ planetSound.volume = 0.5;
 
 icon.addEventListener("mouseenter", function() {
     console.log("MOUSE ENTER");
+    if (!document.body.classList.contains("active")) {
+        return;
+    }
 
     animation.beginElement();
 
+    planetSound.currentTime = 0;
+    planetSound.play();
+});
+
+icon.addEventListener("click", function() {
+    console.log("PLANET CLICK");
+    document.body.classList.add("active");
+    animation.beginElement();
     planetSound.currentTime = 0;
     planetSound.play();
 });
